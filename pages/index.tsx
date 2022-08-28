696eb657cc1fa6919f0756812771d7c757cd707e
Aelpxy <aelpxy@protonmail.com>
2022-08-28 16:58:02 +0000

    format using prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,22 +14,22 @@ const Home: NextPage = () => {
       <div>
         <HelloWorld />
         <div>
-        <div className="flex flex-col space-y-4 m-20">
-        <p className="text-lg text-center">Get started by</p>
-          <span className="font-mono text-center">
-            npx degit aelpxy/nextjs-template
-          </span>
-        </div>
-        <div className="flex flex-col space-y-4 m-20">
-        <p className="text-lg text-center mb-15">Deployed on</p>
-          <Image
-            src={"/vercel.svg"}
-            alt={"Vercel"}
-            loading={"lazy"}
-            width={"100px"}
-            height={"100px"}
-          />
-        </div>
+          <div className="flex flex-col space-y-4 m-20">
+            <p className="text-lg text-center">Get started by</p>
+            <span className="font-mono text-center">
+              npx degit aelpxy/nextjs-template
+            </span>
+          </div>
+          <div className="flex flex-col space-y-4 m-20">
+            <p className="text-lg text-center mb-15">Deployed on</p>
+            <Image
+              src={"/vercel.svg"}
+              alt={"Vercel"}
+              loading={"lazy"}
+              width={"100px"}
+              height={"100px"}
+            />
+          </div>
         </div>
       </div>
     </div>
